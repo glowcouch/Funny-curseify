2e83a41a076a494e2a12808395ff581d095727b9
glowcouch <42631203+glowcouch@users.noreply.github.com>
2021-10-21 17:33:05 +1300

    Format

--- processing.py
+++ processing.py
@@ -1,17 +1,18 @@
 def replaceWithInArray(array, replace, replaceWith):
-  for i in range(0, len(array)):
-    if array[i] == replace:
-      array[i] = replaceWith
-  return(array)
+    for i in range(0, len(array)):
+        if array[i] == replace:
+            array[i] = replaceWith
+    return(array)
+
 
 def curseify(string):
 
-  words = string.split(" ")
-  output = ""
-  #replace all instances of oops with balderdash
-  replaceWithInArray(words, "oops", "balderdash")
+    words = string.split(" ")
+    output = ""
+    
+    # replace all instances of oops with balderdash
+    replaceWithInArray(words, "oops", "balderdash")
 
-  for i in range(0, len(words)):
-    output += words[i] + " "
-
-  return output
+    for i in range(0, len(words)):
+        output += words[i] + " "
+    return output
