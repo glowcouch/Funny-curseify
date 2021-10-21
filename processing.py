ffc8c6de3240c1e67504734cb303e167ac8c69e7
glowcouch <42631203+glowcouch@users.noreply.github.com>
2021-10-21 18:43:26 +1300

    Update processing.py

--- processing.py
+++ processing.py
@@ -9,10 +9,15 @@ def curseify(string):
 
     words = string.split(" ")
     output = ""
-    
+
     # replace all instances of oops with balderdash
     replaceWithInArray(words, "oops", "balderdash")
 
+    # add son of a monkey to you are a
+    for i in range(0, len(words)):
+        if words[i] == "you" & words[i+1] == "are" & words[i+2] == "a":
+            for i2 in range(i, len(words) - i):
+                
     for i in range(0, len(words)):
         output += words[i] + " "
     return output
