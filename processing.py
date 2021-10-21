e653aaccaa587e1f8c2a0746fdf83a4ce7fb356b
glowcouch <42631203+glowcouch@users.noreply.github.com>
2021-10-22 06:50:27 +1300

    Fixed son of a monkey and balderdash
    Forgot to include other instances of oops, like capitals and punctuation.

--- processing.py
+++ processing.py
@@ -12,16 +12,24 @@ def curseify(string):
 
     # replace all instances of oops with balderdash
     replaceWithInArray(words, "oops", "balderdash")
+    replaceWithInArray(words, "Oops", "Balderdash")
+    replaceWithInArray(words, "oops,", "balderdash,")
+    replaceWithInArray(words, "Oops,", "Balderdash,")
 
     # add son of a monkey to you are a
     for i in range(0, len(words)):
         if str(words[i]).lower() == "you" and str(words[i+1]).lower() == "are" and str(words[i+2]).lower() == "a":
             for i2 in range(i+3, len(words)):
-                words[i2] = ""
+                if "." in words[i2]:
+                    words[i2] = ""
+                    break
+                else:
+                    words[i2] = ""
             words[i+3] = "son of a monkey."
 
     for i in range(0, len(words)):
-        output += words[i] + " "
+        if words[i] != "":
+            output += words[i] + " "
         #if words[i] != "":
         #    if "." in words and words[i] != "." and words[i+1] != ".":
         #        output += words[i] + " "
